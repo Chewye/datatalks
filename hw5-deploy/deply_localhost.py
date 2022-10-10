9155e2c732b85cfcb844178c75743532b53e6ec9
Chewye <werfa13q@gmail.com>
2022-10-10 22:58:55 +0500

    upload homework #5

---
+++ hw5-deploy/deply_localhost.py
@@ -0,0 +1,17 @@
+import requests
+
+
+url = 'http://localhost:9696/predict'
+
+customer_id = 'xyz-123'
+customer = {"reports": 0, "share": 0.001694, "expenditure": 0.12, "owner": "yes"}
+
+
+response = requests.post(url, json=customer).json()
+print(response)
+
+
+if response['churn'] == True:
+    print('sending promo email to %s' %customer_id)
+else:
+    print('Not sending promo email to %s' %customer_id)
---
+++ hw5-deploy/request.py
@@ -0,0 +1,17 @@
+import requests
+
+
+url = 'http://localhost:9696/predict'
+
+customer_id = 'xyz-123'
+client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
+
+
+response = requests.post(url, json=client).json()
+print(response)
+
+
+if response['churn'] == True:
+    print('sending promo email to %s' %customer_id)
+else:
+    print('Not sending promo email to %s' %customer_id)
