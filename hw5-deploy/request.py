8749df7061f1724a579d8e65a22f79371ba7984f
Chewye <werfa13q@gmail.com>
2022-10-10 23:49:14 +0500

    fix

--- hw5-deploy/request.py
+++ hw5-deploy/request.py
@@ -4,7 +4,7 @@ import requests
 url = 'http://localhost:9696/predict'
 
 customer_id = 'xyz-123'
-client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
+client = {"reports": 0, "share": 0.001694, "expenditure": 0.12, "owner": "yes"}
 
 
 response = requests.post(url, json=client).json()
